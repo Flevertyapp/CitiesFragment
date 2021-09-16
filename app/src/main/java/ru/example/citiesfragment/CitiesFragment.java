b04701d19a5bf5527b71f34c2eb764ed32fbb61d
Flevertyapp <Flevertyap@gmail.com>
2021-09-17 01:03:08 +0300

    add show coat of arms for landscape orientation

--- app/src/main/java/ru/example/citiesfragment/CitiesFragment.java
+++ app/src/main/java/ru/example/citiesfragment/CitiesFragment.java
@@ -1,6 +1,7 @@
 package ru.example.citiesfragment;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,58 +12,16 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CitiesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class CitiesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CitiesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CitiesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CitiesFragment newInstance(String param1, String param2) {
-        CitiesFragment fragment = new CitiesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private boolean isLandscape;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cities, container, false);
     }
 
@@ -79,19 +38,49 @@ public class CitiesFragment extends Fragment {
         String[] cities = getResources().getStringArray(R.array.cities);
         //создание текствью, заполнение и добавление на экран
         for (int i = 0; i < cities.length; i++) {
-            String city=cities[i];
-            TextView tv=new TextView(getContext());
+            String city = cities[i];
+            TextView tv = new TextView(getContext());
             tv.setText(city);
             tv.setTextSize(30);
             layoutView.addView(tv);
-            final int fi=i;
+            final int fi = i;
             tv.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    showRortCoatOfArms(fi);
+                    showCoatOfArms(fi);
                 }
             });
         }
+    }
+
+    //активити создана, можно ее использовать
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //проверка конфигурации экрана
+        isLandscape = getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE;
+        if(isLandscape){
+            showLandCoatOfArms(0);
+        }
+    }
+
+    private void showCoatOfArms(int index) {
+        if(isLandscape){
+            showLandCoatOfArms(index);
+        }else showRortCoatOfArms(index);
+    }
+
+    private void showLandCoatOfArms(int index) {
+        //создание нового фрагмента с позицией для вывода герба
+        CoatOfArmsFragment detail = CoatOfArmsFragment.newInstance(index);
+        //транзакция по замене фрагмента
+        FragmentManager fragmentManager= requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.coat_of_arms,detail); //замена фрагмента
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
     }
 
     private void showRortCoatOfArms(int index) {
